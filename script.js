e7110f4801ab5c1e8058a76d44493d349703fd16
Caleb <calebmcgee06@gmail.com>
2024-07-26 12:03:24 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/* const Book = function (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function (){ 
+        return `${title} by ${author}, ${pages} pages, ${read}`;
+    }
+    
+};
+
+let supper = new Book('Supper', 'CM', 100, 'read');
+
+console.log(supper.info());
+*/
+/*
+function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+    this.sayName = function() {
+      console.log(this.name)
+    };
+  }
+  
+    const player1 = new Player('steve', 'X');
+    const player2 = new Player('also steve', 'O'); 
+
+    console.log(Object.getPrototypeOf(player1) === Player.prototype);
+    console.log(Object.getPrototypeOf(player2) === Player.prototype);
+*/
+
